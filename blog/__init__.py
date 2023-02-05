ef58e4bfc08bd771a435d327bb5062e9889ccebe
Thiago Xavier <thiago.xavieraguilar1@gmail.com>
2023-02-05 18:21:52 -0300

    commit

--- blog/__init__.py
+++ blog/__init__.py
@@ -3,37 +3,53 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+#initialize the SQLAlchemy object for database management
 db = SQLAlchemy()
+#set the name of the database to be used
 DB_NAME = "blog.db"
 
-
+#define the main factory function for creating the Flask app
 def create_app():
+    #create a new Flask app instance
     app = Flask(__name__)
+    #set the secret key for the app
     app.config['SECRET_KEY'] = "blog"
+    #set the URI of the SQLAlchemy database to be used by the app
     app.config['SQL_ALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    #initialize the SQLAlchemy object 
     db.init_app(app)
 
+    #import the views and auth blueprints
     from .views import views
     from .auth import auth
-
+    
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    
     from .models import User
 
+    #create the database for the app
     create_database(app)
 
+  
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
+    #initialize the LoginManager
     login_manager.init_app(app)
 
+    
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
 
+    #return the Flask app instance
     return app
 
+# function for creating the database for the app
 def create_database(app):
+    #check if the database file exists
     if not path.exists("website/" + DB_NAME):
+        #create the database tables if the file does not exist
         db.create_all(app=app)
         print("Created database!")
--- blog/auth.py
+++ blog/auth.py
@@ -11,18 +11,23 @@ auth = Blueprint("auth", __name__)
 @auth.route("/login", methods=['GET','POST'])
 def login():
     if request.method == 'POST':
+        #get the email and password from the request form
         email = request.form.get("email")
         password = request.form.get("password")
-        #check if user exist
+        #check if user exist in the database
         user = User.query.filter_by(email=email).first()
         if user:
+            #check password matches the hashed password in the databas
             if check_password_hash(user.password, password):
+                #if the password is correct, log the user in and redirect them to the home page
                 flash("Logged in!", category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
+                #if the password is incorrect, show an error message
                 flash('Password is incorrect.', category='error')
         else:
+            #if email does not exist, show an error message
             flash('Email does not exist.', category='error')
 
     return render_template("login.html")
@@ -30,15 +35,18 @@ def login():
 
 @auth.route("/sign-up", methods=['GET','POST'])
 def sign_up():
+    #get the email, username, password, and password confirmation from the request form
     if request.method == 'POST':
         email = request.form.get("email")
         username = request.form.get("username")
         password = request.form.get("password")
         confirm_password = request.form.get("confirm_password")
         
+        #check if the email or username already exists in the database
         email_exists = User.query.filter_by(email=email).first()
         username_exists = User.query.filter_by(username=username).first()
 
+        #validate the input
         if email_exists:
             flash('Email is already in use.', category='error')
         elif username_exists:
@@ -52,8 +60,8 @@ def sign_up():
         elif len(email) < 4:
             flash("Email is invalid.", category='error')
         else:
-            new_user = User(email=email, username=username, password=generate_password_hash(
-                password, method='sha256'))
+            #if the input is valid, create a new user, log them in, and redirect them to the home page
+            new_user = User(email=email, username=username, password=generate_password_hash(password, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
